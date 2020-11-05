6fc4eec6747daf589a4c0d01f5d3ac662850438c
김젼 <jiyaaany@gmail.com>
2020-11-05 18:38:31 +0900

    레슨장 사이트 프론트 작업

---
+++ src/views/Notices.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+        <p class="h4 mt-3 ml-3">공지사항</p>
+        <b-list-group class="p-3">
+            <Notice v-for="notice in notices" :notice="notice" :key="notice.id"></Notice>
+        </b-list-group>
+    </div>
+</template>
+
+<script>
+import Notice from '@/components/Notice.vue';
+
+export default {
+    data: (vue) => ({
+        notices: [
+        {
+            id: 1,
+            title: 'title1',
+            content: 'content1',
+            reg_date: vue.$moment().format('YYYY-MM-DD H:m:s')
+        },
+        {
+            id: 2,
+            title: 'title2',
+            content: 'content2',
+            reg_date: vue.$moment().format('YYYY-MM-DD H:m:s')
+        },
+        {
+            id: 3,
+            title: 'title3',
+            content: 'content3',
+            reg_date: vue.$moment().format('YYYY-MM-DD H:m:s')
+        },
+        {
+            id: 4,
+            title: 'title4',
+            content: 'content4',
+            reg_date: vue.$moment().format('YYYY-MM-DD H:m:s')
+        },
+        ]
+  }),
+
+  components: { Notice }
+}
+</script>
---
+++ src/views/Schedule.vue
@@ -0,0 +1,91 @@
+<template>
+    <div>
+        <b-calendar block locale="ko-KR" v-model="selectedYMD"></b-calendar>
+        <div v-if="selectedYMD" class="p-3 d-flex flex-wrap">
+            <Lesson v-for="lesson in lessons" :key="lesson.id" :lesson="lesson" ></Lesson>
+        </div>
+    </div>    
+</template>
+
+<script>
+import Lesson from '@/components/Lesson.vue';
+
+export default {
+    data: () => ({
+        selectedYMD: '',
+        lessons: [
+            {
+                id: 1,
+                date: '2020-10-15',
+                time: '7:00',
+                totalCnt: 1,
+                usableCnt: 1,
+            },
+            {
+                id: 2,
+                date: '2020-10-15',
+                time: '12:00',
+                totalCnt: 1,
+                usableCnt: 1,
+            },
+            {
+                id: 3,
+                date: '2020-10-15',
+                time: '17:00',
+                totalCnt: 1,
+                usableCnt: 0,
+            },
+            {
+                id: 4,
+                date: '2020-10-16',
+                time: '7:00',
+                totalCnt: 1,
+                usableCnt: 0,
+            },
+            {
+                id: 5,
+                date: '2020-10-16',
+                time: '10:00',
+                totalCnt: 1,
+                usableCnt: 1,
+            },
+            {
+                id: 6,
+                date: '2020-10-16',
+                time: '12:00',
+                totalCnt: 1,
+                usableCnt: 1,
+            },
+            {
+                id: 7,
+                date: '2020-10-17',
+                time: '10:00',
+                totalCnt: 1,
+                usableCnt: 1,
+            },
+            {
+                id: 8,
+                date: '2020-10-17',
+                time: '17:00',
+                totalCnt: 1,
+                usableCnt: 0,
+            },
+            {
+                id: 9,
+                date: '2020-10-17',
+                time: '19:00',
+                totalCnt: 1,
+                usableCnt: 0,
+            },
+        ]
+    }),
+
+    components: { Lesson },
+
+    watch: {
+        selectedYMD(){
+            
+        }
+    }
+}
+</script>
